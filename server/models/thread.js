dc0e0b9025995866dfbe1b99ac71cbb7da71f9c4
Unic-X <armansinghkshatri69@gmail.com>
2024-10-08 20:11:21 +0530

    Added models and Admin

---
+++ server/models/thread.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose")
+
+const ThreadSchema = new mongoose.Schema({
+  board: { type: String, required: true },
+  subject: String,
+  content: { type: String, required: true },
+  image: String,
+  created: { type: Date, default: Date.now },
+  lastBump: { type: Date, default: Date.now },
+  replies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Reply' }],
+  posterID: String,
+  locked: { type: Boolean, default: false },
+  sticky: { type: Boolean, default: false }
+});
+
+const Thread = mongoose.model('Thread', ThreadSchema);
+
+
+module.exports = Thread
